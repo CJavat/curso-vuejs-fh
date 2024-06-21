aa9706ba58b13dea5ae30e83789a80c82c236260
CJavatX <cdpm98@hotmail.com>
2024-06-21 12:47:12 -0600

    SECCIÓN 9 / CAP 6 - EN CURSO

---
+++ 05-vue-spa/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <a href="#/">Home</a>
+    <a href="#/features">Features</a>
+    <a href="#/pricing">Pricing</a>
+    <a href="#/contact">Contact</a>
+  </nav>
+
+  <main class="bg-slate-100">
+    <RouterView />
+  </main>
+</template>
+
+<script setup lang="ts">
+  
+
+</script>
