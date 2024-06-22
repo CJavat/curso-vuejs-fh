434f4af75bb10528a783f6239d8c9b8195defec5
cjavat <cdpm98@hotmail.com>
2024-06-22 06:48:56 -0600

    SECCIÓN 9 - TERMINADO

--- 05-vue-spa/src/router/index.ts
+++ 05-vue-spa/src/router/index.ts
@@ -1,29 +1,73 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 import HomePage from "@/modules/landing/pages/HomePage.vue";
+import NotFound404 from "@/modules/common/pages/NotFound404.vue";
+import isAuthenticatedGuard from '@/modules/auth/guards/is-authenticated.guard';
 
 const router = createRouter({
-  history: createWebHashHistory( import.meta.env.BASE_URL ),
+  history: createWebHistory( import.meta.env.BASE_URL ),
   routes: [
-    {
+    { // Landing
       path: '/',
-      name: 'home',
-      component: HomePage
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomePage
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import("@/modules/landing/pages/FeaturesPage.vue")
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import("@/modules/landing/pages/PricingPage.vue")
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import("@/modules/landing/pages/ContactPage.vue")
+        },
+        {
+          path: '/pokemon/:id',
+          name: 'pokemon',
+          beforeEnter: [ isAuthenticatedGuard ],
+          props: ( route ) => {
+            const id = Number( route.params.id );
+
+            return isNaN( id ) ? { id: 1 } : { id };
+          },
+          component: () => import("@/modules/pokemons/pages/PokemonPage.vue")
+        },
+      ]
     },
-    {
-      path: '/features',
-      name: 'features',
-      component: () => import("@/modules/landing/pages/FeaturesPage.vue")
+    { // Auth
+      path: '/auth',
+      redirect: '/auth/login',
+      name: 'auth',
+      component: () => import("@/modules/auth/layout/AuthLayout.vue"),
+      children: [
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import("@/modules/auth/pages/LoginPage.vue"),
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import("@/modules/auth/pages/RegisterPage.vue"),
+
+        }
+      ]
     },
-    {
-      path: '/pricing',
-      name: 'pricing',
-      component: () => import("@/modules/landing/pages/PricingPage.vue")
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: () => import("@/modules/landing/pages/ContactPage.vue")
-    },
+    { // Not FOund
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFound404
+    }
   ]
 });
 
